99a61572847b06e8feb32969fe298ff95a67a988
Samrat <ksamrat224@gmail.com>
2025-12-21 19:49:23 +0545

    refactor:  adding new models to the Prisma client

--- lib/generated/prisma/browser.ts
+++ lib/generated/prisma/browser.ts
@@ -17,4 +17,28 @@ import * as Prisma from './internal/prismaNamespaceBrowser'
 export { Prisma }
 export * as $Enums from './enums'
 export * from './enums';
-
+/**
+ * Model Test
+ * 
+ */
+export type Test = Prisma.TestModel
+/**
+ * Model User
+ * 
+ */
+export type User = Prisma.UserModel
+/**
+ * Model Session
+ * 
+ */
+export type Session = Prisma.SessionModel
+/**
+ * Model Account
+ * 
+ */
+export type Account = Prisma.AccountModel
+/**
+ * Model Verification
+ * 
+ */
+export type Verification = Prisma.VerificationModel
