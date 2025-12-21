d96969d3a173345ce50eea3442bddd40c9327a0c
Samrat <ksamrat224@gmail.com>
2025-12-21 18:22:57 +0545

    feat: add GitHub OAuth provider to auth configuration

--- lib/auth.ts
+++ lib/auth.ts
@@ -7,4 +7,11 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql", // or "mysql", "postgresql", ...etc
   }),
+  socialProviders:{
+    github:{
+        clientId:process.env.GITHUB_CLIENT_ID!,
+        clientSecret:process.env.GITHUB_CLIENT_SECRET!,
+        scope:["repo"]
+    }
+  }
 });
