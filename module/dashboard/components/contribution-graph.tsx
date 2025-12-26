cd60fb2a9bdfd59f576119bdeaa85f0f9d1ff162
Samrat <ksamrat224@gmail.com>
2025-12-26 08:53:38 +0545

    refactor: update contribution graph to use ActivityCalendar and add getContributionStats action

---
+++ module/dashboard/components/contribution-graph.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+import { ActivityCalendar } from "react-activity-calendar";
+import { useTheme } from "next-themes";
+
+const ContributionGraph = () => {
+  return <div>ContributionGraph</div>;
+};
+
+export default ContributionGraph;
