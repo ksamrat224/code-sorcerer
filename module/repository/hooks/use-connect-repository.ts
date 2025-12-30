02d915c93b9626182cf1232bb2b308938cc12f82
Samrat <ksamrat224@gmail.com>
2025-12-30 19:05:14 +0545

    refactor: add query invalidation and toast on repository connect hook

--- module/repository/hooks/use-connect-repository.ts
+++ module/repository/hooks/use-connect-repository.ts
@@ -1,5 +1,32 @@
-"use client"
+"use client";
 
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { connectRepository } from "../actions";
 import { toast } from "sonner";
+import { error } from "console";
+
+export const useConnectRepository = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async ({
+      owner,
+      repo,
+      githubId,
+    }: {
+      owner: string;
+      repo: string;
+      githubId: number;
+    }) => {
+      return await connectRepository(owner, repo, githubId);
+    },
+
+    onSuccess: () => {
+      toast.success("Repository connected successfully"),
+        queryClient.invalidateQueries({ queryKey: ["repositories"] });
+    },
+    onError: () => {
+      toast.error("Failed to connect repository");
+      console.error(error);
+    },
+  });
+};
