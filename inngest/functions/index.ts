75c3b7fc16510a663567a15a29e4f6239e19a188
Samrat <ksamrat224@gmail.com>
2026-01-05 20:36:51 +0545

    feat: add repository indexing on connect with GitHub file fetch

--- inngest/functions/index.ts
+++ inngest/functions/index.ts
@@ -1,10 +1,28 @@
+import prisma from "@/lib/db";
 import { inngest } from "../../inngest/client";
+import { getRepoFileContents } from "@/module/github/lib/github";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
+export const indexRepo = inngest.createFunction(
+  { id: "index-repo" },
+  { event: "repository.connected" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+    const { owner, repo, userId } = event.data;
+    //will fetch files from the repo
+    const files = await step.run("fetch-files", async () => {
+      const account = await prisma.account.findFirst({
+        where: {
+          userId: userId,
+          providerId: "github",
+        },
+      });
+      if (!account?.accessToken) {
+        throw new Error("No github access token found");
+      }
+
+      return await getRepoFileContents(account.accessToken, owner, repo);
+    });
+    await step.run("index-codebase", async () => {
+      //await indexCodebase
+    });
   }
 );
