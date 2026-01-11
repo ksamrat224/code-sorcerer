f37b7fa55e02a51cbbb94ae01fc069fc1cc96d2c
Samrat <ksamrat224@gmail.com>
2026-01-11 21:27:02 +0545

    fix: enforce session check in getReviews

--- module/review/actions/index.ts
+++ module/review/actions/index.ts
@@ -3,4 +3,11 @@ import prisma from "@/lib/db";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 
-export async function getReviews() {}
+export async function getReviews() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
+    throw new Error("Unauthorized");
+  }
+}
